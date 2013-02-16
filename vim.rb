8b9cb394dec8d795e39aae91926c282ce5fe6b08
Chris Hoffman <cehoffman@gmail.com>
2013-02-16 03:58:08 -0600

    Update vim

--- vim.rb
+++ vim.rb
@@ -1,7 +1,7 @@
 class Vim < Coresys::Formula
-  url 'https://github.com/b4winckler/vim.git', tag: 'v7-3-789'
+  url 'https://github.com/b4winckler/vim.git', tag: 'v7-3-820'
   homepage 'http://www.vim.org/'
-  version '7.3.789'
+  version '7.3.820'
 
   def install
     system './configure', "--prefix=#{prefix}",
@@ -12,7 +12,7 @@ class Vim < Coresys::Formula
                           "--enable-multibyte",
                           "--with-tlib=ncursesw",
                           "--enable-pythoninterp",
-                          "--enable-rubyinterp",
+                          "--enable-rubyinterp=dynamic",
                           "--with-features=huge"
     system 'make'
     system 'make', 'install'
