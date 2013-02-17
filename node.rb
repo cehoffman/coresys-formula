1db5f84dd2cf3463acd4e7224ff8ba595053bb68
Chris Hoffman <cehoffman@gmail.com>
2013-02-17 08:37:35 -0600

    Update node

--- node.rb
+++ node.rb
@@ -1,6 +1,6 @@
 class Node < Coresys::Formula
-  url 'http://nodejs.org/dist/v0.6.15/node-v0.6.15.tar.gz'
-  digest :sha1, '5c52d8f91f221277fe69d0e5bf73d13d0f23038f'
+  url 'http://nodejs.org/dist/v0.8.17/node-v0.8.19.tar.gz'
+  sha1 '65d22e4e183cee8888c797300d8fdbb5c530c740'
 
   def install
     system './configure', "--prefix=#{prefix}"
