d959a387d5363e67081fa17cfad0bb64c463f96b
Chris Hoffman <cehoffman@gmail.com>
2013-02-18 09:10:18 -0600

    Update stderred

--- stderred.rb
+++ stderred.rb
@@ -1,9 +1,9 @@
 class Stderred < Coresys::Formula
-  url 'https://github.com/cehoffman/stderred.git'
+  url 'https://github.com/sickill/stderred.git'
   version 'HEAD'
 
   def install
-    system 'make', 'lib64/stderred.so'
-    lib.install 'lib64/stderred.so' => 'libstderred.so'
+    system 'make', '64'
+    lib.install 'lib64/libstderred.so'
   end
 end
